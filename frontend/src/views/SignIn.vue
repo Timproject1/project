eabfb19978760dac69ba8916d3aefbcbe4b63dc8
mxnh <fun@yedam.ac>
2026-02-23 16:00:49 +0900

    스타일변경

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,162 +1,145 @@
 <template>
-  <navbar btnBackground="bg-gradient-success" />
-  <div
-    class="page-header align-items-start min-vh-100"
-    style="
-      background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');
-    "
-  >
-    <span class="mask bg-gradient-dark opacity-6"></span>
-    <div class="container my-auto">
+  <div class="bg-white">
+    <div class="container top-0 position-sticky z-index-sticky">
       <div class="row">
-        <div class="col-lg-4 col-md-8 col-12 mx-auto">
-          <div class="card z-index-0 fadeIn3 fadeInBottom">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+        <div class="col-12">
+          <navbar
+            isBlur="blur my-3 py-2 mt-4 start-0 end-0 mx-4 shadow blur border-radius-lg"
+          />
+        </div>
+      </div>
+    </div>
+    <main class="mt-0 main-content">
+      <section>
+        <div class="page-header min-vh-100">
+          <div class="container">
+            <div class="row">
               <div
-                class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1"
+                class="col-6 d-lg-flex d-none h-100 my-auto pe-0 ps-0 position-absolute top-0 start-0 text-center justify-content-center flex-column"
               >
-                <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">
-                  Sign in
-                </h4>
-                <div class="row mt-3">
-                  <div class="col-2 text-center ms-auto">
-                    <a class="btn btn-link px-3" href="javascript:;">
-                      <i class="fab fa-facebook text-white text-lg"></i>
-                    </a>
+                <div
+                  class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center"
+                  style="
+                    background-image: url(&quot;https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80&quot;);
+                    background-size: cover;
+                  "
+                ></div>
+              </div>
+
+              <div
+                class="col-xl-5 col-lg-6 col-md-8 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5"
+              >
+                <div class="card card-plain mt-8">
+                  <div class="card-body">
+                    <form
+                      role="form"
+                      class="text-start"
+                      @submit.prevent="handleLogin"
+                    >
+                      <p
+                        v-if="loginError"
+                        class="text-danger text-xs font-weight-bolder mb-3"
+                      >
+                        * 입력된 정보를 확인해주십시오.
+                      </p>
+
+                      <div class="custom-login-row mb-4">
+                        <label class="login-label">아이디</label>
+                        <material-input
+                          v-model="user_id"
+                          id="user_id"
+                          type="text"
+                          variant="outline"
+                          class="flex-grow-1"
+                        />
+                      </div>
+
+                      <div class="custom-login-row mb-4">
+                        <label class="login-label">비밀번호</label>
+                        <material-input
+                          v-model="user_pw"
+                          id="password"
+                          type="password"
+                          variant="outline"
+                          class="flex-grow-1"
+                        />
+                      </div>
+
+                      <div class="text-center">
+                        <material-button
+                          class="my-2"
+                          variant="gradient"
+                          color="success"
+                          fullWidth
+                          size="lg"
+                          >로 그 인</material-button
+                        >
+                      </div>
+
+                      <div class="text-center">
+                        <router-link :to="{ name: 'SignUp' }">
+                          <material-button
+                            class="my-2"
+                            variant="gradient"
+                            color="success"
+                            fullWidth
+                            size="lg"
+                            >회원가입</material-button
+                          >
+                        </router-link>
+                      </div>
+                    </form>
                   </div>
-                  <div class="col-2 text-center px-1">
-                    <a class="btn btn-link px-3" href="javascript:;">
-                      <i class="fab fa-github text-white text-lg"></i>
-                    </a>
-                  </div>
-                  <div class="col-2 text-center me-auto">
-                    <a class="btn btn-link px-3" href="javascript:;">
-                      <i class="fab fa-google text-white text-lg"></i>
-                    </a>
+                  <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                    <p class="mb-4 text-sm mx-auto text-end">
+                      <a
+                        href="javascript:;"
+                        class="text-warning text-gradient font-weight-bold"
+                        >아이디/비밀번호 찾기</a
+                      >
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="card-body">
-              <form role="form" class="text-start mt-3">
-                <div class="mb-3">
-                  <material-input
-                    id="email"
-                    type="email"
-                    label="Email"
-                    name="email"
-                  />
-                </div>
-                <div class="mb-3">
-                  <material-input
-                    id="password"
-                    type="password"
-                    label="Password"
-                    name="password"
-                  />
-                </div>
-                <material-switch id="rememberMe" name="rememberMe"
-                  >Remember me</material-switch
-                >
-                <div class="text-center">
-                  <material-button
-                    class="my-4 mb-2"
-                    variant="gradient"
-                    color="success"
-                    fullWidth
-                    >Sign in</material-button
-                  >
-                </div>
-                <p class="mt-4 text-sm text-center">
-                  Don't have an account?
-                  <router-link
-                    :to="{ name: 'SignUp' }"
-                    class="text-success text-gradient font-weight-bold"
-                    >Sign up</router-link
-                  >
-                </p>
-              </form>
-            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <footer class="footer position-absolute bottom-2 py-2 w-100">
-      <div class="container">
-        <div class="row align-items-center justify-content-lg-between">
-          <div class="col-12 col-md-6 my-auto">
-            <div class="copyright text-center text-sm text-white text-lg-start">
-              © {{ new Date().getFullYear() }}, made with
-              <i class="fa fa-heart" aria-hidden="true"></i> by
-              <a
-                href="https://www.creative-tim.com"
-                class="font-weight-bold text-white"
-                target="_blank"
-                >Creative Tim</a
-              >
-              for a better web.
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <ul
-              class="nav nav-footer justify-content-center justify-content-lg-end"
-            >
-              <li class="nav-item">
-                <a
-                  href="https://www.creative-tim.com"
-                  class="nav-link text-white"
-                  target="_blank"
-                  >Creative Tim</a
-                >
-              </li>
-              <li class="nav-item">
-                <a
-                  href="https://www.creative-tim.com/presentation"
-                  class="nav-link text-white"
-                  target="_blank"
-                  >About Us</a
-                >
-              </li>
-              <li class="nav-item">
-                <a
-                  href="https://www.creative-tim.com/blog"
-                  class="nav-link text-white"
-                  target="_blank"
-                  >Blog</a
-                >
-              </li>
-              <li class="nav-item">
-                <a
-                  href="https://www.creative-tim.com/license"
-                  class="nav-link pe-0 text-white"
-                  target="_blank"
-                  >License</a
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
 import Navbar from "@/examples/PageLayout/Navbar.vue";
 import MaterialInput from "@/components/MaterialInput.vue";
-import MaterialSwitch from "@/components/MaterialSwitch.vue";
 import MaterialButton from "@/components/MaterialButton.vue";
 import { mapMutations } from "vuex";
 
 export default {
-  name: "sign-in",
+  name: "SignIn",
   components: {
     Navbar,
     MaterialInput,
-    MaterialSwitch,
     MaterialButton,
   },
-  beforeMount() {
+  data() {
+    return {
+      user_id: "",
+      user_pw: "",
+      loginError: false, // 시안의 빨간 에러 메시지 제어
+    };
+  },
+  methods: {
+    ...mapMutations(["toggleEveryDisplay", "toggleHideConfig"]),
+    handleLogin() {
+      if (!this.user_id || !this.user_pw) {
+        this.loginError = true;
+      } else {
+        alert("로그인 시도: " + this.user_id);
+      }
+    },
+  },
+  mounted() {
     this.toggleEveryDisplay();
     this.toggleHideConfig();
   },
@@ -164,8 +147,23 @@ export default {
     this.toggleEveryDisplay();
     this.toggleHideConfig();
   },
-  methods: {
-    ...mapMutations(["toggleEveryDisplay", "toggleHideConfig"]),
-  },
 };
 </script>
+
+<style scoped>
+/* 시안 image_9e52fa.png 레이아웃 재현 */
+.custom-login-row {
+  display: flex;
+  align-items: center;
+}
+.login-label {
+  width: 100px;
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: #344767;
+  margin-bottom: 0;
+}
+.text-warning {
+  color: #fb8c00 !important; /* 찾기 링크 색상 */
+}
+</style>
--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -6,7 +6,7 @@
           <navbar
             isBlur="blur my-3 py-2 mt-4 start-0 end-0 mx-4 shadow blur border-radius-lg"
             btnBackground="bg-gradient-success"
-            v-bind:darkMode="true"
+            :darkMode="true"
           />
         </div>
       </div>
@@ -21,65 +21,140 @@
               >
                 <div
                   class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center"
-                  :style="{
-                    backgroundImage:
-                      'url(' +
-                      require('@/assets/img/illustrations/illustration-signin.jpg') +
-                      ')',
-                  }"
+                  style="
+                    background-image: url(&quot;https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80&quot;);
+                    background-size: cover;
+                  "
                 ></div>
               </div>
               <div
-                class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5"
+                class="col-xl-5 col-lg-6 col-md-8 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5"
               >
-                <div class="card card-plain">
-                  <div class="pb-0 card-header bg-transparent mb-4">
-                    <h4 class="font-weight-bolder">Sign In</h4>
-                    <p class="mb-0">
-                      Enter your email and password to register
-                    </p>
+                <div class="card card-plain mt-5">
+                  <div class="pb-0 card-header bg-transparent mb-2 text-start">
+                    <h4 class="font-weight-bolder">회원가입</h4>
                   </div>
                   <div class="card-body">
-                    <form role="form">
-                      <div class="mb-3">
-                        <material-input
-                          id="name"
-                          type="text"
-                          label="Name"
-                          name="name"
-                          size="lg"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <material-input
-                          id="email"
-                          type="email"
-                          label="Email"
-                          name="email"
-                          size="lg"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <material-input
-                          id="password"
-                          type="password"
-                          label="Password"
-                          name="password"
-                          size="lg"
-                        />
-                      </div>
-                      <material-checkbox
-                        id="flexCheckDefault"
-                        class="font-weight-light"
-                        checked
-                      >
-                        I agree the
-                        <a
-                          href="../../../pages/privacy.html"
-                          class="text-dark font-weight-bolder"
-                          >Terms and Conditions</a
+                    <form role="form" @submit.prevent="handleRegister">
+                      <div class="custom-row mb-3">
+                        <label class="custom-label">아이디</label>
+                        <div class="custom-content">
+                          <material-input
+                            v-model="form.user_id"
+                            id="user_id"
+                            type="text"
+                            variant="outline"
+                          />
+                          <small v-if="errors.idExists" class="error-msg"
+                            >* 사용할 수 없는 아이디입니다.</small
+                          >
+                        </div>
+                        <material-button
+                          type="button"
+                          color="success"
+                          variant="gradient"
+                          size="sm"
+                          class="ms-2 mb-0"
+                          @click="checkId"
+                          >중복 확인</material-button
                         >
-                      </material-checkbox>
+                        <span class="required-text">(필수)</span>
+                      </div>
+
+                      <div class="custom-row mb-3">
+                        <label class="custom-label">비밀번호</label>
+                        <div class="custom-content">
+                          <material-input
+                            v-model="form.user_pw"
+                            id="password"
+                            type="password"
+                            variant="outline"
+                          />
+                        </div>
+                        <span class="required-text">(필수)</span>
+                      </div>
+
+                      <div class="custom-row mb-3">
+                        <label class="custom-label">비밀번호 확인</label>
+                        <div class="custom-content">
+                          <material-input
+                            v-model="form.confirm_pw"
+                            id="confirm_pw"
+                            type="password"
+                            variant="outline"
+                          />
+                          <small v-if="isPwMismatch" class="error-msg"
+                            >* 비밀번호가 틀렸습니다</small
+                          >
+                        </div>
+                        <span class="required-text">(필수)</span>
+                      </div>
+
+                      <div class="custom-row mb-3">
+                        <label class="custom-label">이름</label>
+                        <div class="custom-content">
+                          <material-input
+                            v-model="form.user_name"
+                            id="name"
+                            type="text"
+                            variant="outline"
+                          />
+                        </div>
+                        <span class="required-text">(필수)</span>
+                      </div>
+
+                      <div class="custom-row mb-3">
+                        <label class="custom-label">연락처</label>
+                        <div class="custom-content">
+                          <material-input
+                            v-model="form.phone"
+                            id="phone"
+                            type="text"
+                            variant="outline"
+                          />
+                        </div>
+                        <span class="required-text">(필수)</span>
+                      </div>
+
+                      <div class="custom-row mb-3">
+                        <label class="custom-label">이메일</label>
+                        <div class="custom-content">
+                          <material-input
+                            v-model="form.email"
+                            id="email"
+                            type="email"
+                            variant="outline"
+                          />
+                          <small v-if="errors.emailExists" class="error-msg"
+                            >* 가입 된 이메일입니다.</small
+                          >
+                        </div>
+                        <span class="required-text">(필수)</span>
+                      </div>
+
+                      <div class="custom-row mb-3">
+                        <label class="custom-label">기관명</label>
+                        <div class="custom-content">
+                          <material-input
+                            v-model="form.org_name"
+                            id="org"
+                            type="text"
+                            variant="outline"
+                            readonly
+                          />
+                        </div>
+                        <material-button
+                          type="button"
+                          color="success"
+                          variant="gradient"
+                          size="sm"
+                          class="ms-2 mb-0"
+                          @click="openOrgSearch"
+                          >검색</material-button
+                        >
+                        <span class="required-text">(필수)</span>
+                      </div>
+
                       <div class="text-center">
                         <material-button
                           class="mt-4"
@@ -87,20 +162,11 @@
                           color="success"
                           fullWidth
                           size="lg"
-                          >Sign Up</material-button
+                          type="submit"
+                          >가입하기</material-button
                         >
                       </div>
                     </form>
-                  </div>
-                  <div class="px-1 pt-0 text-center card-footer px-lg-2">
-                    <p class="mx-auto mb-4 text-sm">
-                      Don't have an account?
-                      <router-link
-                        :to="{ name: 'SignIn' }"
-                        class="text-success text-gradient font-weight-bold"
-                        >Sign In</router-link
-                      >
-                    </p>
                   </div>
                 </div>
               </div>
@@ -113,33 +179,92 @@
 </template>
 
 <script>
+// 2. 컴포넌트 경로를 프로젝트 구조에 맞춰 정확히 지정
 import Navbar from "@/examples/PageLayout/Navbar.vue";
 import MaterialInput from "@/components/MaterialInput.vue";
-import MaterialCheckbox from "@/components/MaterialCheckbox.vue";
 import MaterialButton from "@/components/MaterialButton.vue";
-const body = document.getElementsByTagName("body")[0];
 import { mapMutations } from "vuex";
 
 export default {
-  name: "sign-up",
+  name: "SignUp",
   components: {
-    Navbar,
+    navbar: Navbar, // 템플릿 태그 <navbar>와 매칭
     MaterialInput,
-    MaterialCheckbox,
     MaterialButton,
   },
-  beforeMount() {
-    this.toggleEveryDisplay();
-    this.toggleHideConfig();
-    body.classList.remove("bg-gray-100");
+  data() {
+    return {
+      form: {
+        user_id: "",
+        user_pw: "",
+        confirm_pw: "",
+        user_name: "",
+        phone: "",
+        email: "",
+        org_name: "",
+      },
+      errors: { idExists: false, emailExists: false },
+    };
   },
-  beforeUnmount() {
-    this.toggleEveryDisplay();
-    this.toggleHideConfig();
-    body.classList.add("bg-gray-100");
+  computed: {
+    isPwMismatch() {
+      return (
+        this.form.user_pw !== this.form.confirm_pw &&
+        this.form.confirm_pw !== ""
+      );
+    },
   },
   methods: {
     ...mapMutations(["toggleEveryDisplay", "toggleHideConfig"]),
+    checkId() {
+      alert("아이디 중복 확인");
+    },
+    openOrgSearch() {
+      window.open("/org-search", "orgSearch", "width=500,height=600");
+    },
+    handleRegister() {
+      alert("가입 완료!");
+    },
+  },
+  mounted() {
+    this.toggleEveryDisplay();
+    this.toggleHideConfig();
   },
 };
 </script>
+
+<style scoped>
+/* 시안의 레이아웃을 위한 스타일 */
+.custom-row {
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+}
+.custom-label {
+  width: 120px;
+  min-width: 120px;
+  padding-top: 10px;
+  font-weight: 700;
+  font-size: 0.85rem;
+  color: #344767;
+  text-align: left;
+}
+.custom-content {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
+.error-msg {
+  color: #f44335;
+  font-size: 0.7rem;
+  margin-top: 2px;
+  text-align: left;
+}
+.required-text {
+  color: #f44335;
+  font-size: 0.7rem;
+  padding-top: 12px;
+  margin-left: 10px;
+  white-space: nowrap;
+}
+</style>
