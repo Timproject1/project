741dbef436344e2df71c43e57067d35cdfaaf3d3
jcwoo1213 <jcwoo1213@gmail.com>
2026-02-23 17:54:49 +0900

    정찬우/260223/로그인 완성 회원가입작성중

--- backend/routes/user_router.js
+++ backend/routes/user_router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const ctrl = require("../controllers/user_controller");
 console.log("route");
-router.post("/login", ctrl.login);
-router.get("/check/:id", ctrl.check);
+router.post("/login", ctrl.login); //로그인
+router.get("/check/:id", ctrl.check); //중복체크
 
 module.exports = router;
