7abf0ee2319006172da51ddc86a4e8382161d41e
mxnh <fun@yedam.ac>
2026-02-24 17:09:58 +0900

    이민호/260224/회원가입창

--- backend/app.js
+++ backend/app.js
@@ -2,42 +2,37 @@ require("dotenv").config();
 const express = require("express");
 const pool = require("./db/mapper.js");
 const cors = require("cors");
-const transporter = require("./util/mail.js");
 const app = express();
 const user_route = require("./routes/user_router.js");
 const center_route = require("./routes/center_router.js");
+
 BigInt.prototype.toJSON = function () {
   return this.toString();
 };
+
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
-});
+// 라우터 설정 (listen 위로 올리는 것이 좋습니다)
 app.use("/center", center_route);
-app.use("/user", user_route); //유저 라우트
+app.use("/user", user_route);
+
 app.get("/", async (req, res) => {
   try {
-    // res.send("test");
     const query = "show tables";
     const result = await pool.query(query);
     res.json(result);
-    // const option = {
-    //   from: process.env.MAIL_USER,
-    //   to: "jcwoo1213@naver.com",
-    //   text: "1234",
-    //   subject: "1234",
-    // };
-    // transporter.sendMail(option, (err, info) => {
-    //   if (err) console.log(err);
-    // });
-    // res.send("test1234");
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: error.message });
   }
 });
 
 app.get("/test", (req, res) => {
-  res.send("test");
+  res.send("test server is running!");
+});
+
+// 서버 시작은 맨 마지막에!
+app.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
 });
