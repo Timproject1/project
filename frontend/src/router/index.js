1e4ad71ea676ad0f29554d2407a5a6ffbe3b966b
jcwoo1213 <jcwoo1213@gmail.com>
2026-02-25 17:18:37 +0900

    정찬우/260226/작성하기 진행중

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -69,7 +69,7 @@ const routes = [
       },
       {
         path: "document",
-        component: () => import("../views/document/documentLIST.vue"),
+        component: () => import("../views/list/documentLIST.vue"),
       },
     ],
   },
