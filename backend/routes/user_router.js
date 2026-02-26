83379e5c753cbce69053c248909b0e2beb3e8d3b
kabus-12 <dksgudwn15@gmail.com>
2026-02-26 17:08:47 +0900

    안형주/260226/우선순위db완료,상담기록db진행중

--- backend/routes/user_router.js
+++ backend/routes/user_router.js
@@ -4,8 +4,13 @@ console.log("route");
 router.post("/login", ctrl.login); //로그인
 router.get("/check/:id", ctrl.check); //중복체크
 router.post("/signup", ctrl.signUp);
-router.post("/priority", ctrl.prioritySetting);
-router.get("/priority/:id", ctrl.priorityApp);
-// router.post("", ctrl.appPriority);
+router.post("/priority", ctrl.prioritySetting); //우선순위 신청
+router.get("/priority/:id", ctrl.priorityApp); //우선순위 신청 데이터 당겨오기
+router.post("/priority_manager", ctrl.appPriority); //우선순위 승인
+router.post("/returnPriority_manager", ctrl.returnPriority); //우선순위 반려
+router.post("/record", ctrl.addrecord); //상담기록 추가
+router.get("/recordlist", ctrl.recordList); //상담기록 출력
+router.get("/modifyRecordlist/:id", ctrl.modifyRecordList); //상담기록 수정내역 출력
+router.post("/saverecord", ctrl.saveRecord); //임시저장
 
 module.exports = router;
