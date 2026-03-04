edb8f56d35676600507652aa081e24bdb3eb0405
jcwoo1213 <jcwoo1213@gmail.com>
2026-03-04 10:37:47 +0900

    정찬우/260304/양식리스트 페이징완료

--- frontend/src/views/form/formList.vue
+++ frontend/src/views/form/formList.vue
@@ -1,6 +1,6 @@
 <script setup>
 // import { useMemberStore } from "@/store/member";
-import { ref } from "vue";
+import { ref, onBeforeMount } from "vue";
 import { useRouter } from "vue-router";
 import axios from "axios";
 
@@ -8,16 +8,37 @@ const router = useRouter();
 // const memberStore = useMemberStore();
 // const searchQuery = ref({ writer: "", maneger: "", sup: "" });
 const list = ref([]);
+const totalCount = ref(0); // 전체 데이터 개수
+const currentPage = ref(1); // 현재 페이지
+const perPage = 10; // 페이지당 항목 수
+const totalPages = ref(0); // 전체 페이지 수
 const formatDate = (dateString) => {
   if (!dateString) return "";
   return new Date(dateString).toLocaleDateString("ko-KR");
   // 결과: "2026. 2. 22."
 };
+const changePage = (page) => {
+  if (page < 1 || page > totalPages.value) return;
+  currentPage.value = page;
+  getList(); // 페이지 바뀔 때마다 다시 호출
+};
 const getList = async () => {
-  const result = await axios.get("http://localhost:3000/form/list");
+  const result = await axios.get("http://localhost:3000/form/list", {
+    params: {
+      page: currentPage.value,
+      limit: perPage,
+    },
+  });
   console.log(result.data);
   list.value = result.data.result;
   console.log(list.value);
+};
+const getCount = async () => {
+  const result = await axios.get("http://localhost:3000/form/count");
+  console.log(result.data);
+  totalCount.value = result.data.count;
+  console.log(totalCount.value);
+  totalPages.value = Math.ceil(Number(totalCount.value) / perPage);
 };
 const getForm = (form_ver) => {
   router.push(`/form/view/${form_ver}`);
@@ -26,7 +47,10 @@ const getForm = (form_ver) => {
 const moveRegister = () => {
   router.push(`/form/write`);
 };
-getList();
+onBeforeMount(async () => {
+  await getCount();
+  await getList();
+});
 </script>
 <template>
   <div class="container-fluid py-4">
@@ -34,7 +58,7 @@ getList();
       <main class="flex-grow-1">
         <div class="card shadow-sm">
           <div class="card-header p-3">
-            <h6 class="mb-0 font-weight-bolder">지원 신청 내역</h6>
+            <h6 class="mb-0 font-weight-bolder">양식 목록</h6>
           </div>
 
           <div class="card-body px-0 pb-2">
@@ -42,11 +66,6 @@ getList();
               <table class="table align-items-center mb-0">
                 <thead>
                   <tr class="bg-gray-100">
-                    <th
-                      class="text-center text-secondary text-xxs font-weight-bolder opacity-7"
-                    >
-                      번호
-                    </th>
                     <th
                       class="ps-4 text-secondary text-xxs font-weight-bolder opacity-7"
                     >
@@ -82,13 +101,10 @@ getList();
                 </thead>
                 <tbody>
                   <tr
-                    v-for="(form, index) in list"
+                    v-for="form in list"
                     :key="form.form_ver"
                     @click="getForm(form.form_ver)"
                   >
-                    <td class="text-center text-sm">
-                      {{ list.length - index }}
-                    </td>
                     <td class="ps-4 text-sm font-weight-bold">
                       {{ form.form_ver }}
                     </td>
@@ -115,11 +131,26 @@ getList();
             <div
               class="d-flex justify-content-between align-items-center p-3 mt-2"
             >
-              <material-pagination>
-                <material-pagination-item prev />
-                <material-pagination-item label="1" active />
-                <material-pagination-item label="2" />
-                <material-pagination-item next />
+              <material-pagination color="success" size="sm">
+                <material-pagination-item
+                  prev
+                  @click="changePage(currentPage - 1)"
+                  :disabled="currentPage === 1"
+                />
+
+                <material-pagination-item
+                  v-for="page in totalPages"
+                  :key="page"
+                  :label="page"
+                  :active="currentPage === page"
+                  @click="changePage(page)"
+                />
+
+                <material-pagination-item
+                  next
+                  @click="changePage(currentPage + 1)"
+                  :disabled="currentPage === totalPages"
+                />
               </material-pagination>
 
               <material-button
