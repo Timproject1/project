9f3970f1f87df0afb391a69ab04ade6718f0be75
kabus-12 <dksgudwn15@gmail.com>
2026-03-06 10:24:34 +0900

    안형주/260306/권한설정,오류수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -38,39 +38,46 @@ const routes = [
     path: "/work",
     name: "work",
     component: work,
-    // meta: { requiredLevel: "a2" },
+    meta: { requiredLevel: ["a2", "a3"] },
     children: [
       {
         path: "plan",
         components: { right: () => import("../views/work/plan.vue") },
+        meta: { requiredLevel: ["a2"] },
       },
       {
         path: "plan_manager",
         components: { right: () => import("../views/work/plan_manager.vue") },
+        meta: { requiredLevel: ["a3"] },
       },
       {
         path: "priority",
         components: {
           right: () => import("../views/work/priority.vue"),
+          meta: { requiredLevel: ["a2"] },
         },
       },
       {
         path: "priority_manager",
         components: {
           right: () => import("../views/work/priority_manager.vue"),
+          meta: { requiredLevel: ["a3"] },
         },
       },
       {
         path: "record",
         components: { right: () => import("../views/work/record.vue") },
+        meta: { requiredLevel: ["a2", "a3"] },
       },
       {
         path: "result",
         components: { right: () => import("../views/work/result.vue") },
+        meta: { requiredLevel: ["a2", "a3"] },
       },
       {
         path: "representative",
         components: { right: () => import("../views/work/representative.vue") },
+        meta: { requiredLevel: ["a2", "a3"] },
       },
     ],
   },
